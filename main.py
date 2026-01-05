ddd7de61f2292f43ccec690a912b1f9bc1e41a54
thasegawa <tomoya.tomoya-2002@ezweb.ne.jp>
2026-01-05 23:43:15 +0900

    テスト

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 
-@app.get("/")
+@app.get('/')
 def read_root():
-    return {"message": "Hello from Windows to AWS"}
+  return {"message": "Hello"}
+
+@app.get('/predict/{value}')
+def predict(value: int):
+  result = 2 * value
+  return {"input": value,
+          "prediction": result,
+          "status": "success"}
